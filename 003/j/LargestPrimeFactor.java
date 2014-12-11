1e76505c277ab877c7de6fb6f2c00ade523015dc
Gustav Henning <ghenning@kth.se>
2014-12-11 23:19:33 +0100

    003

---
+++ 003/j/LargestPrimeFactor.java
@@ -0,0 +1,41 @@
+package j;
+
+import java.util.ArrayList;
+
+public class LargestPrimeFactor {
+	
+	public LargestPrimeFactor(){
+		ArrayList<Long> primes = new ArrayList<Long>();
+		long f = 600851475143L;
+		long i = 0;
+		do {
+			i = factorOf(f);
+			if(f % i == 0 && !primes.contains(i)){
+				primes.add(i);
+				f = f / i;
+			}
+		} while(i != f);
+		primes.add(f);
+		
+		long largest = 0;
+		for(long p : primes){
+			if(p > largest){
+				largest = p;
+			}
+		}
+		System.out.println(largest);
+		
+	}
+	
+	public long factorOf(long p){
+		for(long i = 2; i < p; i++){
+			if(p % i == 0){
+				return i;
+			}
+		}
+		return p;
+	}
+	public static void main(String[] args){
+		new LargestPrimeFactor();
+	}
+}
